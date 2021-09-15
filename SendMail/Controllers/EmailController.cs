7ccfe7d65a9518b6ace8821da2f66989124dcab8
Fehmin1905 <70522742+Fehmin1905@users.noreply.github.com>
2021-09-15 13:37:54 +0400

    First Commit

---
+++ SendMail/Controllers/EmailController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendMail.Model;
+using SendMail.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendMail.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IMailService _mailService;
+
+        public EmailController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> Send([FromForm] MailRequest request)
+        {
+            try
+            {
+                await _mailService.SendEmailAsync(request);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
